e4528d7278b8b6024bf30a0ef945376d7fdd50e8
KosmoKos <pro.konstant@gmail.com>
2016-06-19 19:28:21 +0300

    commit_1

--- src/main/java/KosmoKos/jba/service/InitDbService.java
+++ src/main/java/KosmoKos/jba/service/InitDbService.java
@@ -1,10 +1,16 @@
 package KosmoKos.jba.service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import KosmoKos.jba.entity.Blog;
+import KosmoKos.jba.entity.Item;
 import KosmoKos.jba.entity.Role;
 import KosmoKos.jba.entity.User;
 import KosmoKos.jba.repository.BlogRepository;
@@ -12,6 +18,7 @@ import KosmoKos.jba.repository.ItemRepository;
 import KosmoKos.jba.repository.RoleRepository;
 import KosmoKos.jba.repository.UserRepository;
 
+@Transactional
 @Service
 public class InitDbService {
 
@@ -37,8 +44,33 @@ public class InitDbService {
 		roleUser.setName("ROLE_ADMIN");
 		roleRepository.save(roleAdmin);
 		
-		User userAmin = new User();
-		userAmin.setName("admin");
+		User userAdmin = new User();
+		userAdmin.setName("admin");
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(roleAdmin);
+		roles.add(roleUser);
+		userAdmin.setRoles(roles);
+		userRepository.save(userAdmin);
+		
+		Blog blogJavavids = new Blog();
+		blogJavavids.setName("JavaVids");
+		blogJavavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+		blogJavavids.setUser(userAdmin);
+		blogRepository.save(blogJavavids);
+		
+		Item item1 = new Item();
+		item1.setBlog(blogJavavids);
+		item1.setTitle("first");
+		item1.setLink("http://www.javavids.com");
+		item1.setPublishedDate(new Date());
+		itemRepository.save(item1);
+		
+		Item item2 = new Item();
+		item2.setBlog(blogJavavids);
+		item2.setTitle("second");
+		item2.setLink("http://www.javavids.com");
+		item2.setPublishedDate(new Date());
+		itemRepository.save(item2);
 	}
 	
 	
