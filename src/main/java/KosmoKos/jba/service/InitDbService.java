2c81cbc8fe62542d44b6d584c274b4e0e5c1b7d4
KosmoKos <pro.konstant@gmail.com>
2016-07-02 15:25:59 +0300

    commit_1

--- src/main/java/KosmoKos/jba/service/InitDbService.java
+++ src/main/java/KosmoKos/jba/service/InitDbService.java
@@ -5,11 +5,10 @@ import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-
+import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import KosmoKos.jba.entity.Blog;
 import KosmoKos.jba.entity.Item;
