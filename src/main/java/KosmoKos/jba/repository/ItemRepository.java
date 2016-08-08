7615c59e07a8f37d3bb72402dd85316d2898b631
KosmoKos <pro.konstant@gmail.com>
2016-08-08 11:00:14 +0300

    commit_1

--- src/main/java/KosmoKos/jba/repository/ItemRepository.java
+++ src/main/java/KosmoKos/jba/repository/ItemRepository.java
@@ -11,7 +11,8 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
-List<Item> findByBlog(Blog blog, Pageable pageable);
-	
+	List<Item> findByBlog(Blog blog, Pageable pageable);
+
+	Item findByBlogAndLink(Blog blog, String link);
 	
 }
--- src/main/java/KosmoKos/jba/service/BlogService.java
+++ src/main/java/KosmoKos/jba/service/BlogService.java
@@ -1,18 +1,43 @@
 package KosmoKos.jba.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import KosmoKos.jba.entity.Blog;
+import KosmoKos.jba.entity.Item;
 import KosmoKos.jba.entity.User;
+import KosmoKos.jba.exception.RssEcxeption;
 import KosmoKos.jba.repository.BlogRepository;
+import KosmoKos.jba.repository.ItemRepository;
 import KosmoKos.jba.repository.UserRepository;
 
 @Service
 public class BlogService {
 
+	@Autowired
+	private RssService rssService;
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	public void saveItems(Blog blog){
+		try {
+			List<Item> items = rssService.getItems(blog.getUrl());
+			for (Item item : items) {
+				Item savedItem = itemRepository.findByBlogAndLink(blog, item.getLink());
+				if(savedItem == null){
+				item.setBlog(blog);
+				itemRepository.save(item);
+				}
+			}
+		} catch (RssEcxeption e) {
+			e.printStackTrace();
+		}
+	}
 		@Autowired
 		private BlogRepository blogRepository;
 		
@@ -23,6 +48,7 @@ public class BlogService {
 		User user =  userRepository.findByName(name);
 		blog.setUser(user);
 		blogRepository.save(blog);
+		saveItems(blog);
 						
 	}
 
