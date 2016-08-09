b1bc9524cc7e44fb51bb8bd65c80105acdf638da
KosmoKos <pro.konstant@gmail.com>
2016-08-09 23:24:35 +0300

    commit_1

--- src/main/java/KosmoKos/jba/service/InitDbService.java
+++ src/main/java/KosmoKos/jba/service/InitDbService.java
@@ -1,7 +1,7 @@
 package KosmoKos.jba.service;
 
 import java.util.ArrayList;
-//import java.util.Date;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -81,7 +81,4 @@ public class InitDbService {
 		}
 	}
 	
-	
-	
-
 }
